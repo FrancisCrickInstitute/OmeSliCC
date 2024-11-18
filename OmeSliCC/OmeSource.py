974f635a8ddca85386872c9bfdfe0d61d07c43e2
Joost de Folter <folterj@gmail.com>
2024-11-18 16:48:07 +0200

    ome-tiff fix: read first translation position

--- OmeSliCC/OmeSource.py
+++ OmeSliCC/OmeSource.py
@@ -100,6 +100,7 @@ class OmeSource:
             if 'PositionZ' in plane:
                 position.append((float(plane.get('PositionZ')), plane.get('PositionZUnit')))
             #c, z, t = plane.get('TheC'), plane.get('TheZ'), plane.get('TheT')
+            break
 
         self.position = position
         self.source_mag = 0
